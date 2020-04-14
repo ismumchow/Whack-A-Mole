b4e9462fd140011818aba0cf0ae45e3a4c65376a
ischdry <ichowdhury4@fordham.edu>
2020-04-13 21:32:24 -0400

    time interval for mole changed

--- app.js
+++ app.js
@@ -9,18 +9,19 @@ let currentTime = timeLeft.textContent;
 
 //we want the mole to be a random square on the grid 
  function  randomSquare() {
-     square.forEach(className => {
-         className.classList.remove('mole'); 
+     square.forEach(box => {  // box is essentially referring to each square while square acts like an array of all the boxes 
+         box.classList.remove('mole'); 
      }); 
+     //random position is essentially the box with the mole classes added to it
      let randomPosition = square[Math.floor(Math.random()* 9)] // math.floor makes sure the num is an int and the * 9 makes it under or equal to 9 
      randomPosition.classList.add('mole');
      //assign the id of the randomPosition to the hit position for us to use later 
      hitPosition = randomPosition.id;
  }
 
- square.forEach(id => {
-     id.addEventListener('mouseup',() => {
-         if(id.id === hitPosition) {
+ square.forEach(box => {
+     box.addEventListener('mouseup',() => {
+         if(box.id === hitPosition) {
             result = result +1;
             score.textContent = result;
          }
@@ -29,7 +30,7 @@ let currentTime = timeLeft.textContent;
 
  function moveMole( ){
      let timerId = null; 
-     timerId = setInterval(randomSquare,1000)
+     timerId = setInterval(randomSquare,500)
  }
 
  moveMole();
